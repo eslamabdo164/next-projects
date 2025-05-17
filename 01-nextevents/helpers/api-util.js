95b77ff3960c18227c19f5623292c9f9ec7f4f1f
eslam <eslammm>
2025-05-17 15:25:03 +0300

    "using dummy data locally set"

--- 01-nextevents/helpers/api-util.js
+++ 01-nextevents/helpers/api-util.js
@@ -1,38 +1,17 @@
-const url = process.env.NEXT_PUBLIC_FIREBASE_DB;
+import { getAllEvents as getDummyEvents, getFeaturedEvents as getDummyFeaturedEvents, getEventById as getDummyEventById, getFilteredEvents as getDummyFilteredEvents } from '../dummy-data';
 
 export async function getAllEvents() {
-  const response = await fetch(url);
-  const data = await response.json();
-  const events = [];
-  for (const key in data) {
-    events.push({
-      id: key,
-      ...data[key],
-    });
-  }
-  return events;
+  return getDummyEvents();
 }
 
 export async function getFeaturedEvents() {
-  const allEvents = await getAllEvents();
-  return allEvents.filter((event) => event.isFeatured);
+  return getDummyFeaturedEvents();
 }
 
 export async function getEventById(id) {
-  const allEvents = await getAllEvents();
-  return allEvents.find((event) => event.id === id);
+  return getDummyEventById(id);
 }
 
 export async function getFilteredEvents(dateFilter) {
-  const allEvents = await getAllEvents();
-  const { year, month } = dateFilter;
-
-  let filteredEvents = allEvents.filter((event) => {
-    const eventDate = new Date(event.date);
-    return (
-      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
-    );
-  });
-
-  return filteredEvents;
+  return getDummyFilteredEvents(dateFilter);
 }
